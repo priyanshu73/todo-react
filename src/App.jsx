b626ce4f96844b3873a1075ed857f411d2b3fd96
priyanshu73 <you@example.com>
2024-08-22 01:03:45 -0400

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react"
+import "./styles.css"
+import { NewTodoForm } from "./NewTodoForm"
+import {TodoList} from "./TodoList"
+
+export default function App(){
+
+  const [ todos, setTodos] = useState( () => {
+    const localValue = localStorage.getItem("ITEMS")
+
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+
+  useEffect ( () => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+
+  function addTodo(title){
+    setTodos ( (currentTodos) => {
+      return [
+        ...currentTodos, {
+          id : crypto.randomUUID(), title, completed : false },
+        ]
+    })
+
+  }
+    
+
+    const toggleTodo = (id, completed) => {
+        setTodos( currentTodos => {
+          return currentTodos.map( todo => {
+            if (todo.id === id){
+              return { ...todo, completed}
+            }
+
+            return todo
+          })
+        } )
+    }
+
+    const deleteTodo = id => {
+      setTodos( currentTodos => {
+        return currentTodos.filter( todo => todo.id !== id
+        )
+      } )
+    }
+
+
+    const deleteSelected = () => {
+      setTodos( currentTodos => {
+       return  currentTodos.filter( todo => !todo.completed)
+      } )
+
+    }
+    
+
+  return ( 
+    <>
+    <NewTodoForm onSubmit = {addTodo} />
+  <h1 className="header"> ToDo List</h1>
+
+  <TodoList todos = { todos} toggleTodo = {toggleTodo} deleteTodo = {deleteTodo} deleteSelected = {deleteSelected} />
+
+  <button className="deleteall" onClick={ () => deleteSelected()}> Delele Selected</button>
+    </>
+
+  )
+}
+
+
+
+                             
+
+
+
+
+
